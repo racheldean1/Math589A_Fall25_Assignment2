4cbe7a6cf3468f2b5cafb4527499ffccd7222e80
Marek Rychlik <rychlik@arizona.edu>
2025-09-22 19:10:07 -0700

    *** empty log message ***

---
+++ general_linear_solver.py
@@ -0,0 +1,14 @@
+def plu_decomposition_in_place(A):
+    # A given rectangular matrix
+    # Upon return, A must contain the PA=LU decomposition (L and U portion).
+    # The calculation must be performed in place
+    # Note: P, Q must be vectors, not 2D arrays
+    return P, Q, A
+
+
+
+def solve(A, B):
+    
+    return N, c
+
+
